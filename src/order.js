223ea2ce81a2c6f27f39f114afb12ab21cc76013
Emily Miles <ms.emily.j.miles@gmail.com>
2023-01-28 09:57:43 -0700

    Add implementation code to pass all tests

--- src/order.js
+++ src/order.js
@@ -1,7 +1,42 @@
+const takeOrder = (order, list) => {
+  if (list.length < 3) {
+    list.push(order);
+  }
+
+  return list;
+};
+
+const refundOrder = (orderNumber, list) => {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].orderNumber === orderNumber) {
+      list.splice(i, 1);
+    }
+  }
+
+  return list;
+};
+
+const listItems = (list) => {
+  const itemNames = list.map(element => element.item);
+
+  return itemNames.join(', ');
+};
+
+const searchOrder = (list, itemName) => {
+  let orderPresent = false;
+
+  list.forEach(element => {
+    if (element.item === itemName) {
+      orderPresent = true;
+    }
+  });
+
+  return orderPresent;
+};
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
-}
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
+};
--- test/order-test.js
+++ test/order-test.js
@@ -8,13 +8,13 @@ var {
   searchOrder
 } = require("../src/order");
 
-describe("order.js", function() {
-  describe("takeOrder", function() {
-    it.skip("should be a function", function() {
+describe("order.js", function () {
+  describe("takeOrder", function () {
+    it("should be a function", function () {
       assert.isFunction(takeOrder);
     });
 
-    it.skip("should add new orders to an order type list", function() {
+    it("should add new orders to an order type list", function () {
       var order1 = {
         orderNumber: 1,
         item: "burger",
@@ -40,7 +40,7 @@ describe("order.js", function() {
       assert.equal(deliveryOrders[1], order2);
     });
 
-    it.skip("should add another order to an order type list", function () {
+    it("should add another order to an order type list", function () {
 
       var order1 = {
         orderNumber: 1,
@@ -76,7 +76,7 @@ describe("order.js", function() {
       assert.equal(takeOutOrders[2], order3);
     });
 
-    it.skip("should not be able to hold more than 3 orders at a time", function() {
+    it("should not be able to hold more than 3 orders at a time", function () {
       var order1 = {
         orderNumber: 12342,
         item: "burger",
@@ -117,14 +117,14 @@ describe("order.js", function() {
       assert.equal(deliveryOrders.length, 3);
       assert.deepEqual(deliveryOrders, [order1, order2, order3]);
     });
-  }); 
+  });
 
-  describe("refundOrder", function() {
-    it.skip("should be a function", function () {
+  describe("refundOrder", function () {
+    it("should be a function", function () {
       assert.isFunction(refundOrder);
     });
 
-    it.skip("should remove an order by order number", function() {
+    it("should remove an order by order number", function () {
       var order1 = {
         orderNumber: 1657,
         item: "burger",
@@ -154,10 +154,10 @@ describe("order.js", function() {
       refundOrder(1657, deliveryOrders);
 
       assert.equal(deliveryOrders.length, 2);
-      assert.deepEqual(deliveryOrders, [order2, order3])
+      assert.deepEqual(deliveryOrders, [order2, order3]);
     });
 
-    it.skip("should remove a different order by order number", function () {
+    it("should remove a different order by order number", function () {
       var order1 = {
         orderNumber: 1241,
         item: "burger",
@@ -187,16 +187,16 @@ describe("order.js", function() {
       refundOrder(2893, deliveryOrders);
 
       assert.equal(deliveryOrders.length, 2);
-      assert.deepEqual(deliveryOrders, [order1, order3])
+      assert.deepEqual(deliveryOrders, [order1, order3]);
     });
   });
 
-  describe("listOrders", function() {
-    it.skip("should be a function", function () {
+  describe("listOrders", function () {
+    it("should be a function", function () {
       assert.isFunction(listItems);
     });
 
-    it.skip("should list out all of the order items by name", function() {
+    it("should list out all of the order items by name", function () {
       var order1 = {
         orderNumber: 1,
         item: "burger",
@@ -227,7 +227,7 @@ describe("order.js", function() {
       assert.deepEqual(items, "burger, blt sandwich, rueben");
     });
 
-    it.skip("should list out different order items by name", function () {
+    it("should list out different order items by name", function () {
       var order1 = {
         orderNumber: 3,
         item: "grilled cheese",
@@ -263,12 +263,12 @@ describe("order.js", function() {
     });
   });
 
-  describe("searchOrder", function() {
-    it.skip("should be a function", function () {
+  describe("searchOrder", function () {
+    it("should be a function", function () {
       assert.isFunction(searchOrder);
     });
 
-    it.skip("should tell us if an order is in the list", function() {
+    it("should tell us if an order is in the list", function () {
       var order1 = {
         orderNumber: 1234,
         item: "burger",
@@ -292,7 +292,7 @@ describe("order.js", function() {
       assert.equal(searchOrder(deliveryOrders, "blt sandwich"), true);
     });
 
-    it.skip("should tell us if an order is in another list", function () {
+    it("should tell us if an order is in another list", function () {
       var order1 = {
         orderNumber: 6357,
         item: "mac and cheese",
@@ -335,7 +335,7 @@ describe("order.js", function() {
 
       var orders1 = [order1, order2];
       var orders2 = [order2, order3, order4];
-      var orders3 = [order1, order2, order3, order4, order5]
+      var orders3 = [order1, order2, order3, order4, order5];
 
       assert.equal(searchOrder(orders1, "mac and cheese"), true);
       assert.equal(searchOrder(orders1, "hawaiian pizza"), false);
