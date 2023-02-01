32efd4cfda7458c9e733ddc0ecd676409e3b56d4
Emily Miles <ms.emily.j.miles@gmail.com>
2023-02-01 16:33:04 -0700

    Add implementation code to pass all tests

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,31 @@
 class Chef {
-  constructor() {
-    
-  }
-}
+  constructor (chefName, restaurantDetails) {
+    this.name = chefName;
+    this.restaurant = restaurantDetails;
+  };
+
+  greetCustomer = (customerName, condition) => {
+    if (condition === false || condition === undefined) {
+      return `Hello, ${customerName}!`;
+    }
+    return `Good morning, ${customerName}!`;
+  };
+
+  checkForFood = (item) => {
+    let returnStatement;
+
+    if (this.restaurant.menus[item.type].length) {
+      this.restaurant.menus[item.type].forEach(element => {
+        if (element.name === item.name) {
+          returnStatement = `Yes, we're serving ${item.name} today!`;
+        }
+      });
+    } else {
+      returnStatement = `Sorry, we aren't serving ${item.name} today.`;
+    }
+
+    return returnStatement;
+  };
+};
 
 module.exports = Chef;
